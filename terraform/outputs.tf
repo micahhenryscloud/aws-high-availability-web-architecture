ff06a872f83898bcd44924672f3bd81155b2acea
micahhenryscloud <micah.henry61@yahoo.co.uk>
2026-06-01 02:35:02 +0100

    Build high availability AWS architecture with Terraform

---
+++ terraform/outputs.tf
@@ -0,0 +1,21 @@
+output "alb_dns_name" {
+  value = aws_lb.app_alb.dns_name
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
+}
+
+output "private_subnet_ids" {
+  value = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
+}
